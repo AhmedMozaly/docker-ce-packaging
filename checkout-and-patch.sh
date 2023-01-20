2a156bea5fa3e079535c56e1007eb7d8a5a19f5b
Ahmed Mozaly <ahmedmozaly@gmail.com>
2023-01-20 20:09:59 +0200

    add solver.go.patch

--- checkout-and-patch.sh
+++ checkout-and-patch.sh
@@ -1,3 +1,2 @@
 make checkout 
-mv -f solver.go ./src/github.com/docker/docker/vendor/github.com/moby/buildkit/solver/llbsolver/solver.go
-mv -f source.go ./src/github.com/docker/docker/vendor/github.com/moby/buildkit/solver/llbsolver/ops/source.go
+git apply solver.go.patch
