10b9123affbc0feb211e4ddc2f09374af495c20d
Ahmed Mozaly <ahmedmozaly@gmail.com>
2023-01-20 16:39:31 +0200

    fix source.go

--- checkout-and-patch.sh
+++ checkout-and-patch.sh
@@ -1,2 +1,3 @@
 make checkout 
 mv -f solver.go ./src/github.com/docker/docker/vendor/github.com/moby/buildkit/solver/llbsolver/solver.go
+mv -f source.go ./src/github.com/docker/docker/vendor/github.com/moby/buildkit/solver/llbsolver/ops/source.go
